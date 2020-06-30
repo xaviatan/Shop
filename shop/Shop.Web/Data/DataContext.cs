07458280340d08b22818d94ff20b0bd01c2a2198
Milton Erick Chambi Bolivar <xaviatan1@gmail.com>
2020-06-30 16:40:08 +0200

    Clases

---
+++ shop/Shop.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Shop.Web.Data
+{
+    using System;
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Data.Entities;
+    public class DataContext:DbContext
+    {
+        public DbSet<Product> Products { set; get; }
+
+        public DataContext(DbContextOptions<DataContext>options): base(options)
+        {
+
+        }
+    }
+}
---
+++ shop/Shop.Web/Data/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Shop.Web.Data.Entities
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Product
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode =false)]
+        public decimal Price { set; get; }
+
+        [Display(Name = "Image")]
+        public string ImageURL { set; get; }
+        [Display(Name ="Last Sale")]
+        public DateTime LastSale { set; get; }
+
+        [Display(Name ="Last Purchase")]
+        public DateTime LastPurchase { set; get; }
+
+        [Display(Name ="Is Avialable?")]
+        public bool IsAvialable { set; get; }
+
+        [DisplayFormat(DataFormatString ="{0:N2}",ApplyFormatInEditMode =false)]
+        public double Stock { set; get; }
+    }
+}
